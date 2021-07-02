5375f6d2b44f211f0ed17d6be2873ddb20827ff8
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-07-02 12:48:39 +0100

    Update PrivateRoute to fix logic issue with authorization redirect

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { useGlobalContext } from '../context';
 
-const PrivateRoute = ({ Component, loginRequired }) => {
-  const { loggedIn } = useGlobalContext();
+const PrivateRoute = ({ Component, loginBool }) => {
+  const id = localStorage.getItem('user_id');
+  console.log('id: ', id);
+  console.log('login required: ', loginBool);
 
-  if (loginRequired) {
+  if (loginBool) {
     return (
       <Route
         render={(props) =>
-          !loggedIn ? <Component {...props} /> : <Redirect to='/login' />
+          id ? <Component {...props} /> : <Redirect to='/login' />
         }
       />
     );
-  } else {
+  } else if (!loginBool) {
     return (
       <Route
         render={(props) =>
-          loggedIn ? <Component {...props} /> : <Redirect to='/' />
+          !id ? <Component {...props} /> : <Redirect to='/' />
         }
       />
     );
